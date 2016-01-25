43aef8a38afe1f60f7443ecf62229d740822b498
gongjr <gjr9596@gmail.com>
2016-01-25 16:05:01 +0800

    测试3

--- app/src/main/java/com/asiainfo/selforder/ui/DishesMenuActivity.java
+++ app/src/main/java/com/asiainfo/selforder/ui/DishesMenuActivity.java
@@ -77,7 +77,7 @@ public class DishesMenuActivity extends mBaseActivity{
     }
 
     private void initData(){
-        //push试试
+        //push试试,测试Author
         EventBus.getDefault().register(this);
         MerchantRegister merchantRegister=(MerchantRegister)mApp.getData(mApp.KEY_GLOABLE_LOGININFO);
         MerchantDesk merchantDesk=(MerchantDesk)mApp.getData(mApp.KEY_GLOABLE_MERCHANTDESk);
